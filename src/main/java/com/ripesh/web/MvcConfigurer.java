8aad46b1c7ee4a4b3da4958c0f8d67be2ab34242
dixanta <dixanta@gmail.com>
2018-06-13 11:50:38 +0545

    first commit

--- src/main/java/com/ripesh/web/MvcConfigurer.java
+++ src/main/java/com/ripesh/web/MvcConfigurer.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
